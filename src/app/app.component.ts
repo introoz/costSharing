f175f58bd8f1f496445f11e52b3a81371ab5f57a
Paweł Adrianowicz (paweadr487) <paweadr487@student.polsl.pl>
2016-11-27 14:55:05 +0100

    router i tabela

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/primeng';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  private items: MenuItem[];
+
+  private activeItem: MenuItem;
+
+  ngOnInit() {
+    this.items = [
+      { label: 'Groups', icon: 'fa-users' },
+      { label: 'Costs', icon: 'fa-money' }
+      // { label: 'Documentation', icon: 'fa-book' },
+      // { label: 'Support', icon: 'fa-support' },
+      // { label: 'Social', icon: 'fa-twitter' }
+    ];
+
+    this.activeItem = this.items[0];
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { DataTableModule, SharedModule, DialogModule, ButtonModule } from 'primeng/primeng';
+import { InputTextModule } from 'primeng/primeng';
+import { MenuModule, TabMenuModule} from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
+import { CostTableComponent } from './cost-table/cost-table.component';
+import { AppRoutingModule } from 'app/app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CostTableComponent       
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    DataTableModule,
+    DialogModule,
+    ButtonModule,
+    InputTextModule,
+    SharedModule,
+    TabMenuModule,    
+    MenuModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
